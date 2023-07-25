d9d3a66890838d57255c2c5ce04688382287d867
vineet dugar <dugar6661@gmail.com>
2023-07-25 14:43:24 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import dalleRoutes from './routes/dalle.routes.js'
+
+dotenv.config()
+
+const app=express();
+app.use(cors());
+app.use(express.json({limit:"100mb"}))
+
+app.use('/api/v1/dalle',dalleRoutes);
+
+app.listen(8080,()=>console.log("Server started on port 8080"))
